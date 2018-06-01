c54e1924eb9670249e924f43eda65ec6527ce7b6
macstudent <macstudent@10.10.150.94>
2018-06-01 16:40:00 -0400

    Applied arithmatic operation

--- src/employeemanagementsystem/EmployeeManagementSystem.java
+++ src/employeemanagementsystem/EmployeeManagementSystem.java
@@ -17,7 +17,7 @@ public class EmployeeManagementSystem {
     {
            Employee e1= new Employee();
                 
-                   e1.employeeId = 1;
+                   /*e1.employeeId = 1;
                 e1.employeename="rupinder";
                 e1.salary=12220;
                 
@@ -25,13 +25,16 @@ public class EmployeeManagementSystem {
                 e2.employeeId=1;
                 e2.employeeName="kaur";
                 e2.salary=12220;
-                e2.display();
+                e2.display();*/
                
                 Employee e3 = new Employee();
                 e3.employeeID = 2;
                 e3.employeeName ="ravinder";
                 e3.salary =12220;
                 e3.display();
+               
+                Arithmatic arithmatic = new Arithmatic();
+                System.out.println(arithmatic.sum(10,100));
                 
                 
      }
