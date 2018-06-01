c177b8a31166ffbc005c94b53e2d98c8ba53a30a
macstudent <macstudent@10.10.150.94>
2018-06-01 16:11:05 -0400

    CREATE E3

--- src/employeemanagementsystem/EmployeeManagementSystem.java
+++ src/employeemanagementsystem/EmployeeManagementSystem.java
@@ -26,6 +26,13 @@ public class EmployeeManagementSystem {
                 e2.employeeName="kaur";
                 e2.salary=12220;
                 e2.display();
+               
+                Employee e3 = new Employee();
+                e3.employeeID = 2;
+                e3.employeeName ="ravinder";
+                e3.salary =12220;
+                e3.display();
+                
                 
      }
     
